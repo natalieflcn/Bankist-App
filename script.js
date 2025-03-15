c5efb8868a2aeeb67655d34e9676cc0edc7ad21e
natalieflcn <natalie.dflcn@gmail.com>
2025-03-14 23:50:46 -0400

    Added functionality to delete accounts

--- script.js
+++ script.js
@@ -184,3 +184,19 @@ btnTransfer.addEventListener('click', function (e) {
   }
   console.log(amount, receiverAcc);
 });
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
